cff3616c1263bc68d806acf7c6de21be62e8b5f9
太田達也 <tatuya2518@gmail.com>
2021-11-26 23:02:14 +0900

    2021-11-26 23:02

--- main.dart
+++ main.dart
@@ -3,79 +3,218 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:developer';
 
-void main() {
-  runApp(StanpCardApp());
+final userProvider = StateProvider((ref) {
+  return FirebaseAuth.instance.currentUser;
+});
+
+final infoTextProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+final emailProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+final passwordProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ProviderScope(child: StanpCardApp()),
+  );
 }
 
 class StanpCardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'StanpCardApp',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomePage());
+      title: 'StanpCardApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
   }
 }
 
 class HomePage extends StatelessWidget {
-  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('スタンプカードアプリ'),
-      ),
-      body: UnAuthPage(),
-      bottomNavigationBar: BottomNav(),
-    );
+    return (FirebaseAuth.instance.currentUser == null)
+        ? UnAuthPage()
+        : QRPage();
   }
 }
 
 class UnAuthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(8),
-              alignment: Alignment.center,
-              child: Text('ログインまたは会員登録をしてください。'),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Container(
-              padding: EdgeInsets.all(8),
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('ログイン'),
-                style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(Size(130, 40.0))),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('スタンプカードアプリ'),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: Text('ログインまたは会員登録をしてください。'),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
                 padding: EdgeInsets.all(8),
                 alignment: Alignment.center,
                 child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('会員登録'),
+                  onPressed: () async {
+                    await Navigator.of(context)
+                        .pushReplacement(MaterialPageRoute(builder: (context) {
+                      return AuthPage();
+                    }));
+                  },
+                  child: Text('ログイン'),
                   style: ButtonStyle(
-                      minimumSize:
-                          MaterialStateProperty.all(Size(130.0, 40.0))),
-                )),
-          ],
+                      minimumSize: MaterialStateProperty.all(Size(130, 40.0))),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      await Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) {
+                        return AuthPage();
+                      }));
+                    },
+                    child: Text('会員登録'),
+                    style: ButtonStyle(
+                        minimumSize:
+                            MaterialStateProperty.all(Size(130.0, 40.0))),
+                  )),
+            ],
+          ),
         ),
       ),
+      bottomNavigationBar: BottomNav(),
+    );
+  }
+}
+
+class AuthPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final infoText = ref.watch(infoTextProvider.state).state;
+    final email = ref.watch(emailProvider.state).state;
+    final password = ref.watch(passwordProvider.state).state;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('スタンプカードアプリ'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () async {
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return UnAuthPage();
+                }),
+              );
+            },
+            icon: Icon(Icons.close_sharp),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  ref.read(emailProvider.state).state = value;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                obscureText: true,
+                onChanged: (String value) {
+                  ref.read(passwordProvider.state).state = value;
+                },
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Text(infoText),
+              ),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: Text('ユーザー登録'),
+                  onPressed: () async {
+                    try {
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final result = await auth.createUserWithEmailAndPassword(
+                          email: email, password: password);
+                      ref.read(userProvider.state).state = result.user;
+                      await Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) {
+                        return QRPage();
+                      }));
+                    } catch (e) {
+                      ref.read(infoTextProvider.state).state =
+                          "登録に失敗しました:${e.toString()}";
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                child: OutlinedButton(
+                  child: Text('ログイン'),
+                  onPressed: () async {
+                    try {
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final result = await auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      ref.read(userProvider.state).state = result.user;
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          return QRPage();
+                        }),
+                      );
+                    } catch (e) {
+                      ref.read(infoTextProvider.state).state =
+                          "ログインに失敗しました:${e.toString()}";
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QRPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('QRコードがここに入ります。'),
     );
   }
 }
