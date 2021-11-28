23a4c8ee7519e3231ff74197f30a064bd2a076cf
太田達也 <tatuya2518@gmail.com>
2021-11-28 11:57:38 +0900

    2021-11-28 11:57

--- page/news.dart
+++ page/news.dart
@@ -19,60 +19,29 @@ class _NewsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [
+      children: const [
         Card(
-          margin: const EdgeInsets.only(
-              top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'ここにショップからのお知らせが入ります。',
-              style: TextStyle(fontSize: 15),
-            ),
+          margin:
+              EdgeInsets.only(top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
+          child: ListTile(
+            title: Text('ここにショップからのお知らせが入ります。'),
+            subtitle: Text('2021-11-28'),
           ),
         ),
         Card(
-          margin: const EdgeInsets.only(
-              top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'ここにショップからのお知らせが入ります。',
-              style: TextStyle(fontSize: 15),
-            ),
+          margin:
+              EdgeInsets.only(top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
+          child: ListTile(
+            title: Text('ここにショップからのお知らせが入ります。'),
+            subtitle: Text('2021-11-28'),
           ),
         ),
         Card(
-          margin: const EdgeInsets.only(
-              top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'ここにショップからのお知らせが入ります。',
-              style: TextStyle(fontSize: 15),
-            ),
-          ),
-        ),
-        Card(
-          margin: const EdgeInsets.only(
-              top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'ここにショップからのお知らせが入ります。',
-              style: TextStyle(fontSize: 15),
-            ),
-          ),
-        ),
-        Card(
-          margin: const EdgeInsets.only(
-              top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'ここにショップからのお知らせが入ります。',
-              style: TextStyle(fontSize: 15),
-            ),
+          margin:
+              EdgeInsets.only(top: 12.0, left: 10.0, bottom: 0.0, right: 10.0),
+          child: ListTile(
+            title: Text('ここにショップからのお知らせが入ります。'),
+            subtitle: Text('2021-11-28'),
           ),
         ),
       ],
